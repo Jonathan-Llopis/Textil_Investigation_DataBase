d7186d78412ab6994889f84c92fa1e77f1688b42
Sergi Casanova Enguix <sergicasanova6798@gmail.com>
2025-01-23 19:58:54 +0100

    aplicaciones

--- Textil_Investigation_DataBase/src/tela/tela.entity.ts
+++ Textil_Investigation_DataBase/src/tela/tela.entity.ts
@@ -1,3 +1,4 @@
+import { AplicacionesEntity } from 'src/aplicaciones/aplicaciones.enttity';
 import { Cac_TecnicasEntity } from 'src/cac_tecnicas/cac_tecnicas.entity';
 import { ComposicionEntity } from 'src/composicion/composicion.entity';
 import { ConservacionEntity } from 'src/conservacion/conservacion.entity';
@@ -19,14 +20,14 @@ export class TelaEntity {
   @Column()
   denominacion: string;
 
-    @ManyToMany(() => UserEntity, (user) => user.telas_favoritas)
-    @JoinTable()
-    users: UserEntity[];
+  @ManyToMany(() => UserEntity, (user) => user.telas_favoritas)
+  @JoinTable()
+  users: UserEntity[];
 
-    // Relación Many-to-Many con Composición
-    @ManyToMany(() => ComposicionEntity, (composicion) => composicion)
-    @JoinTable()
-    composiciones: ComposicionEntity[];
+  // Relación Many-to-Many con Composición
+  @ManyToMany(() => ComposicionEntity, (composicion) => composicion)
+  @JoinTable()
+  composiciones: ComposicionEntity[];
 
   //Relación Many-to-Many con Tipo Estructural
   @ManyToMany(
@@ -36,29 +37,38 @@ export class TelaEntity {
   @JoinTable()
   tipo_estructurales: TipoEstructuralEntity[];
 
-    // Relación Many-to-Many con Características Técnicas
-    @ManyToMany(() => Cac_TecnicasEntity, (caracteristicaTecnica) => caracteristicaTecnica.telas)
-    @JoinTable()
-    caracteristicas_tecnicas: Cac_TecnicasEntity[];
+  @ManyToMany(() => AplicacionesEntity, (aplicaciones) => aplicaciones.telas)
+  @JoinTable()
+  aplicaciones: AplicacionesEntity[];
 
-    // // Relación Many-to-Many con Características Visuales
-    // @ManyToMany(() => CaracteristicaVisual, (caracteristicaVisual) => caracteristicaVisual.telas)
-    // @JoinTable()
-    // caracteristicas_visuales: CaracteristicaVisual[];
+  // Relación Many-to-Many con Características Técnicas
+  @ManyToMany(
+    () => Cac_TecnicasEntity,
+    (caracteristicaTecnica) => caracteristicaTecnica.telas,
+  )
+  @JoinTable()
+  caracteristicas_tecnicas: Cac_TecnicasEntity[];
 
-    // // Relación Many-to-Many con Aplicaciones
-    // @ManyToMany(() => Aplicacion, (aplicacion) => aplicacion.telas)
-    // @JoinTable()
-    // aplicaciones: Aplicacion[];
+  // // Relación Many-to-Many con Características Visuales
+  // @ManyToMany(() => CaracteristicaVisual, (caracteristicaVisual) => caracteristicaVisual.telas)
+  // @JoinTable()
+  // caracteristicas_visuales: CaracteristicaVisual[];
 
-    // Relación Many-to-Many con Conservación
-    @ManyToMany(() => ConservacionEntity, (conservacion) => conservacion.telas)
-    @JoinTable()
-    conservaciones: ConservacionEntity[];
+  // // Relación Many-to-Many con Aplicaciones
+  // @ManyToMany(() => Aplicacion, (aplicacion) => aplicacion.telas)
+  // @JoinTable()
+  // aplicaciones: Aplicacion[];
 
-    // Relación Many-to-Many con Estructura y Ligamento
-    @ManyToMany(() => EstructuraLigamentosEntity, (estructuraLigamento) => estructuraLigamento.telas)
-    @JoinTable()
-    estructura_ligamentos: EstructuraLigamentosEntity[];
+  // Relación Many-to-Many con Conservación
+  @ManyToMany(() => ConservacionEntity, (conservacion) => conservacion.telas)
+  @JoinTable()
+  conservaciones: ConservacionEntity[];
 
+  // Relación Many-to-Many con Estructura y Ligamento
+  @ManyToMany(
+    () => EstructuraLigamentosEntity,
+    (estructuraLigamento) => estructuraLigamento.telas,
+  )
+  @JoinTable()
+  estructura_ligamentos: EstructuraLigamentosEntity[];
 }
