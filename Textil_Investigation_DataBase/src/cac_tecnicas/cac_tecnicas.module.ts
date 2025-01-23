6f684d375a8bfff16b3058e019da81635cf662b1
Andreu <andreusm1707@gmail.com>
2025-01-23 19:59:13 +0100

    Corregido errores

--- Textil_Investigation_DataBase/src/cac_tecnicas/cac_tecnicas.module.ts
+++ Textil_Investigation_DataBase/src/cac_tecnicas/cac_tecnicas.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UtilsModule } from '../utils/utils.module';
 import { Cac_TecnicasService } from './cac_tecnicas.service';
 import { Cac_TecnicasController } from './cac_tecnicas.controller';
-import { Cac_Tecnicas } from './cac_tecnicas.entity';
+import { Cac_TecnicasEntity } from './cac_tecnicas.entity';
 
 @Module({
-  imports: [UtilsModule, TypeOrmModule.forFeature([Cac_Tecnicas])],
+  imports: [UtilsModule, TypeOrmModule.forFeature([Cac_TecnicasEntity])],
   controllers: [Cac_TecnicasController],
   providers: [Cac_TecnicasService],
 })
