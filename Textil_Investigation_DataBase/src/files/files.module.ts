54d9faea8279c63e510268da76373209335e5c81
Andreu <andreusm1707@gmail.com>
2025-02-03 19:50:15 +0100

    Errores corregidos

--- Textil_Investigation_DataBase/src/files/files.module.ts
+++ Textil_Investigation_DataBase/src/files/files.module.ts
@@ -1,22 +1,23 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { MulterModule } from '@nestjs/platform-express';
 import { GridFsMulterConfigService } from './multer-config.service';
 import { FilesService } from '././files.service';
-import { UsersService } from '../users/users.service';
 import { UtilsModule } from '../utils/utils.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../users/users.entity';
+import { TelaModule } from 'src/tela/tela.module';
 
 @Module({
   imports: [
     MulterModule.registerAsync({
       useClass: GridFsMulterConfigService,
     }),
+    forwardRef(() => TelaModule),
     UtilsModule,
     TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [FilesController],
-  providers: [GridFsMulterConfigService, FilesService, UsersService],
+  providers: [GridFsMulterConfigService, FilesService],
 })
 export class FilesModule {}
